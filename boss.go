495158e1bcc2ada53cbde1bbcddeda3954bb4349
alexalreadytaken <alex.shemelev22@gmail.com>
2022-05-16 21:37:51 +0500

    improvements

--- boss.go
+++ boss.go
@@ -2,30 +2,44 @@ package bossworker
 
 import (
 	"log"
+	"sync"
 	"time"
 )
 
-type Worker[Action any, Response any] func(Action) Response
+type Worker[Event any, Response any] func(Event) Response
 
-type Boss[Action any, Response any] struct {
-	WorkersCount   uint
-	WorkerLifetime time.Duration
-	Actions        chan Action
-	Responses      chan Response
-	Worker         Worker[Action, Response]
-	actionsQueue   []Action
+type Boss[Event any, Response any] struct {
+	MaxWorkersCount uint
+	WorkerLifetime  time.Duration
+	Input           chan Event
+	Output          chan Response
+	Worker          Worker[Event, Response]
+	eventsQueue     []Event
+	eventsMutex     sync.Mutex
 }
 
-func (boss *Boss[Action, Response]) Run() {
-	activeWorkers := make(chan struct{}, boss.WorkersCount)
+func (boss *Boss[Event, Response]) Run() {
+	go boss.listenEvents()
+	boss.handleEvents()
+}
+
+func (boss *Boss[Event, Response]) listenEvents() {
 	for {
 		select {
-		case newAction := <-boss.Actions:
+		case newAction := <-boss.Input:
 			boss.enqueue(newAction)
+		}
+	}
+}
+
+func (boss *Boss[Event, Response]) handleEvents() {
+	activeWorkers := make(chan struct{}, boss.MaxWorkersCount)
+	for {
+		select {
 		case activeWorkers <- struct{}{}:
-			// how
 			if !boss.haveActions() {
 				<-activeWorkers
+				time.Sleep(time.Second) //maybe sleep in default: ?
 				continue
 			}
 			go boss.execute(boss.dequeue(), activeWorkers)
@@ -33,18 +47,20 @@ func (boss *Boss[Action, Response]) Run() {
 	}
 }
 
-func (boss *Boss[Action, Response]) haveActions() bool {
-	return len(boss.actionsQueue) != 0
+func (boss *Boss[Event, Response]) haveActions() bool {
+	boss.eventsMutex.Lock()
+	defer boss.eventsMutex.Unlock()
+	return len(boss.eventsQueue) != 0
 }
 
-func (boss *Boss[Action, Response]) execute(action Action, activeWorkers chan struct{}) {
+func (boss *Boss[Event, Response]) execute(event Event, activeWorkers chan struct{}) {
 	subResponse := make(chan Response)
 	go func() {
-		subResponse <- boss.Worker(action)
+		subResponse <- boss.Worker(event)
 	}()
 	select {
 	case newResp := <-subResponse:
-		boss.Responses <- newResp
+		boss.Output <- newResp
 		log.Println("worker done")
 		<-activeWorkers
 		return
@@ -55,12 +71,16 @@ func (boss *Boss[Action, Response]) execute(action Action, activeWorkers chan st
 	}
 }
 
-func (boss *Boss[Action, Response]) enqueue(action Action) {
-	boss.actionsQueue = append(boss.actionsQueue, action)
+func (boss *Boss[Event, Response]) enqueue(event Event) {
+	boss.eventsMutex.Lock()
+	boss.eventsQueue = append(boss.eventsQueue, event)
+	boss.eventsMutex.Unlock()
 }
 
-func (boss *Boss[Action, Response]) dequeue() Action {
-	elem := boss.actionsQueue[0]
-	boss.actionsQueue = boss.actionsQueue[1:]
+func (boss *Boss[Event, Response]) dequeue() Event {
+	boss.eventsMutex.Lock()
+	defer boss.eventsMutex.Unlock()
+	elem := boss.eventsQueue[0]
+	boss.eventsQueue = boss.eventsQueue[1:]
 	return elem
 }
--- examples/main.go
+++ examples/main.go
@@ -13,32 +13,32 @@ type Response struct {
 }
 
 func main() {
-	actions := make(chan int)
+	events := make(chan int)
 	responses := make(chan Response)
 
-	go produceValues(actions)
+	go produceValues(events)
 	go consumeValues(responses)
 	boss := bossworker.Boss[int, Response]{
-		WorkersCount:   3,
-		WorkerLifetime: time.Second * 2,
-		Actions:        actions,
-		Responses:      responses,
-		Worker:         worker,
+		MaxWorkersCount: 3,
+		WorkerLifetime:  time.Second * 4,
+		Input:           events,
+		Output:          responses,
+		Worker:          worker,
 	}
 	boss.Run()
 }
 
 func worker(req int) Response {
 	log.Println("work")
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Millisecond * 200)
 	return Response{
-		val: req ^ 2,
+		val: req * 2,
 		err: nil,
 	}
 }
 
 func produceValues(actions chan int) {
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 10; i++ {
 		log.Println("produce")
 		time.Sleep(time.Millisecond * 100)
 		actions <- 20
